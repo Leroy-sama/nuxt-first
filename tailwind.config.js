08143cf29af7b473c1b359857f225bbf10b06877
Leroy <ntruderleroy@gmail.com>
2024-09-21 19:13:09 +0300

    Recipe app setup

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"dodgeroll-gold": "#f791fa",
+				"apple-green": "#046e1b",
+				"dire-wolf": "#292727",
+			},
+		},
+		fontFamily: {
+			Montserrat: "Montserrat, sans-serif",
+		},
+		container: {
+			center: true,
+			padding: "1rem",
+			screens: {
+				sm: "640px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1280px",
+			},
+		},
+	},
+	plugins: [],
+};
